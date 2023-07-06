594c5c81cd0efb51a93e554e0dc47d3cf63b2f9e
pedro-valdez <pedro.valdez.dev@protonmail.com>
2023-07-06 02:18:55 -0500

    Added styling to the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Shelf from "./sketchbook/Shelf";
 import { getLatestSlugs } from "./sketchbook/utils/fs";
 
@@ -6,21 +7,27 @@ export default async function Home() {
 
   return (
     <main>
-			<div>
+			<div className="sm:px-8 md:px-16 lg:max-w-prose lg:mx-auto xl:max-w-screen-xl">
 				<div>
-					<header className="prose">
+					<header className="prose px-4 py-8 sm:px-0">
 						<h2>Featured</h2>
 						<p>My favorite sketches</p>
 					</header>
 					<Shelf slugs={["truchet-tiles", "warping-dots", "conways-game-of-life"]}/>
 				</div>
 
-				<div>
-					<header className="prose">
+				<div className="mt-32">
+					<header className="prose px-4 py-8 sm:px-0">
 						<h2>Latest</h2>
 						<p>Fresh sketches out the oven</p>
 					</header>
 					<Shelf slugs={latest}/>
+					<div className="mt-32 flex justify-end px-8">
+						<Link
+							href="/sketchbook/all"
+							className="btn link normal-case ml-auto"
+						>View all</Link>
+					</div>
 				</div>
 			</div>
     </main>
