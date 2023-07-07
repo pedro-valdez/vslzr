308c4409145923e3f52ff8fe936bc2aedad6a650
pedro-valdez <pedro.valdez.dev@protonmail.com>
2023-07-07 12:22:09 -0500

    Refactored sorting function

--- app/sketchbook/utils/fs.ts
+++ app/sketchbook/utils/fs.ts
@@ -2,20 +2,30 @@ import dayjs from "dayjs"
 import { readdir } from "fs/promises"
 
 export type Slug = string
+type ArticleFileMeta = {
+	title: string,
+	date: string,
+	description: string,
+}
+export type ArticleMeta = {
+	slug: Slug,
+	meta: ArticleFileMeta,
+}
 
 const sketchesPath = "app/sketchbook/sketches"
 
-export function getSketchSlugs() {
-	return readdir(sketchesPath)
+export function getSketchSlugs(): Promise<Slug[]> {
+	const slugs: Promise<Slug[]> = readdir(sketchesPath)
+	return slugs
 }
 
-export async function getArticleMetas(slugs: Slug[]) {
+export async function getArticleMetas(slugs: Slug[]): Promise<ArticleMeta[]> {
 	const articleImports = slugs.map(slug => (
 		import(`app/sketchbook/sketches/${slug}/article.mdx`)
 		.then(mod => mod.meta)
 	))
-	const metas = await Promise.all(articleImports)
-	const articles = metas.map((meta, index) => ({
+	const metas: ArticleFileMeta[] = await Promise.all(articleImports)
+	const articles: ArticleMeta[] = metas.map((meta, index) => ({
 		meta,
 		slug: slugs[index],
 	}))
@@ -23,16 +33,18 @@ export async function getArticleMetas(slugs: Slug[]) {
 	return articles
 }
 
-export async function getLatestSlugs() {
+export function sortByDate(a: string, b: string) {
+	const dateA = dayjs(a)
+	const dateB = dayjs(b)
+
+	if (dateA.isSame(dateB)) { return 0 }
+	return dateA.isAfter(dateB) ? -1 : 1
+}
+
+export async function getLatestSlugs(): Promise<Slug[]> {
 	const slugs = await getSketchSlugs()
 	const metas = await getArticleMetas(slugs)
-	metas.sort((a, b) => {
-		const dateA = dayjs(a.meta.date)
-		const dateB = dayjs(b.meta.date)
-
-		if (dateA.isSame(dateB)) { return 0 }
-		return dateA.isAfter(dateB) ? -1 : 1
-	})
+	metas.sort((a, b) => sortByDate(a.meta.date, b.meta.date))
 	const latestSlugs = metas
 		.map(meta => meta.slug)
 		.slice(0, 5)
