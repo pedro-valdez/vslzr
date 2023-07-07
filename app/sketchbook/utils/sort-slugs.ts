d2964088047443e20c201244a85d7961c990fce5
pedro-valdez <pedro.valdez.dev@protonmail.com>
2023-07-07 12:51:15 -0500

    Added script to create slugs json
    The script creates a sorted by date list of slugs. I will use this to make an API.

---
+++ app/sketchbook/utils/sort-slugs.ts
@@ -0,0 +1,15 @@
+import { writeFile } from "fs/promises"
+import { getArticleMetas, getSketchSlugs, sortByDate } from "./fs"
+
+async function main() {
+	const allSlugs = await getSketchSlugs()
+	const allMetas = await getArticleMetas(allSlugs)
+	allMetas.sort((a, b) => sortByDate(a.meta.date, b.meta.date))
+	const sortedSlugs = allMetas.map(meta => meta.slug)
+	const json = JSON.stringify(sortedSlugs)
+
+	await writeFile("app/sketchbook/utils/sorted-slugs.json", json)
+	console.log("here")
+}
+
+main()
