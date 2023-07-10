66e0925f6c35e441cb64e5159e07eaebb4a3b2e6
pedro-valdez <pedro.valdez.dev@protonmail.com>
2023-07-10 15:57:24 -0500

    Created Footer component

---
+++ app/Footer.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+
+export default function Footer() {
+	return (
+		<footer className="p-4 mt-4">
+			<div className="flex flex-col justify-center items-center opacity-50">
+				<div className="space-x-4">
+					<Link href="/about">
+						About
+					</Link>
+					<Link href="/sketchbook/all">
+						Sketches
+					</Link>
+					<Link href="/">
+						VSLZR
+					</Link>
+				</div>
+				<p className="mt-4">© 2023 Pedro Valdez</p>
+			</div>
+		</footer>
+	)
+}
