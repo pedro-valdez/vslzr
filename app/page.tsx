e45bd5bd17470ae58ca9b90ecfded65e37b2fbeb
pedro-valdez <pedro.valdez.dev@protonmail.com>
2023-07-10 16:01:42 -0500

    Updated page with new components

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,32 @@
-import Link from "next/link";
-import Shelf from "./sketchbook/Shelf";
-import { getLatestSlugs } from "./sketchbook/utils/fs";
+import { getArticleMetas, getLatestArticleMetas } from "./sketchbook/utils/fs";
+import FeaturedShelf from "./Shelf/Featured";
+import Shelf from "./Shelf";
 
 export default async function Home() {
-	const latest = await getLatestSlugs()
+	const featuredArticles = await getArticleMetas(["truchet-tiles", "warping-dots", "conways-game-of-life"])
+	const latestArticles = await getLatestArticleMetas()
 
   return (
-    <main>
-			<div className="sm:px-8 md:px-16 lg:max-w-prose lg:mx-auto xl:max-w-screen-xl">
-				<div>
-					<header className="prose px-4 py-8 sm:px-0">
-						<h2>Featured</h2>
-						<p>My favorite sketches</p>
-					</header>
-					<Shelf slugs={["truchet-tiles", "warping-dots", "conways-game-of-life"]}/>
-				</div>
+		<main>
+			<h1 className="sr-only">VSLZR</h1>
 
-				<div className="mt-32">
-					<header className="prose px-4 py-8 sm:px-0">
-						<h2>Latest</h2>
-						<p>Fresh sketches out the oven</p>
-					</header>
-					<Shelf slugs={latest}/>
-					<div className="mt-32 flex justify-end px-8">
-						<Link
-							href="/sketchbook/all"
-							className="btn link normal-case ml-auto"
-						>View all</Link>
-					</div>
+			<div className="px-4">
+				<div className="space-y-24">
+					<section>
+						<h2 className="sr-only">Featured</h2>
+
+						<FeaturedShelf features={featuredArticles} />
+					</section>
+
+					<section>
+						<header className="prose ml-4">
+							<h2 className="mb-0">The Latest</h2>
+							<p className="mb-4">Freshly baked sketches</p>
+						</header>
+						<Shelf articles={latestArticles}/>
+					</section>
 				</div>
 			</div>
-    </main>
+		</main>
   )
 }
