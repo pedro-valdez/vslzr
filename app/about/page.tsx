b18385e4cff6f35acf380e1d6abfb03705821380
pedro-valdez <pedro.valdez.dev@protonmail.com>
2023-07-12 01:47:51 -0500

    Added padding

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,7 +3,7 @@ import About from "./about.mdx"
 export default function AboutPage() {
 	return (
 		<main>
-			<div className="mx-auto prose">
+			<div className="mx-auto prose px-8">
 				<About />
 			</div>
 		</main>
